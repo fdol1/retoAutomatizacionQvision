d1cda2f7a0441cf39a118704ed405b17b640aa62
lpinedau <lpinedau@choucairtesting.com>
2021-02-07 23:02:57 -0500

    Se elimina constante no utilizada

--- src/main/java/com/certificacion/lfpu/utils/Constantes.java
+++ src/main/java/com/certificacion/lfpu/utils/Constantes.java
@@ -5,7 +5,5 @@ import java.util.logging.Logger;
 public class Constantes {
 
     public static int TOTAL_DIAS;
-    public static final Logger logger = Logger.getLogger(Constantes.class.getName());
-    public static final String ELEMENTO_NO_ENCONTRADO = "No se pudo encontrar el elemento: ";
-
+    public static final Logger LOGGER = Logger.getLogger(Constantes.class.getName());
 }
